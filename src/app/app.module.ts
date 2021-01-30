0ea5c8b320a82915bd8309cacad6a6010ac95c6e
Raul Castro <raulc27@gmail.com>
2021-01-29 22:29:34 -0300

    modificando locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePT from '@angular/common/locales/pt';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-
-import {FooterComponent} from './footer/footer.component';
 import { ExercicioDataBindingComponent } from './exercicio-data-binding/exercicio-data-binding.component';
 import { ExercicioDiretivasComponent } from './exercicio-diretivas/exercicio-diretivas.component';
 import { ExercicioPipesComponent } from './exercicio-pipes/exercicio-pipes.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
+
+registerLocaleData(localePT,'pt')
 
 @NgModule({
   declarations: [
@@ -26,7 +29,10 @@ import { ExercicioPipesComponent } from './exercicio-pipes/exercicio-pipes.compo
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{
+    provide:LOCALE_ID,
+    useValue:'pt'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
