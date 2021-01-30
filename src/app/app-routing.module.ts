34d5e7b5ae71935fa2992f8d09beaa7220efa11d
Raul Castro <raulc27@gmail.com>
2021-01-30 17:26:11 -0300

    adicionando guard esta logado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,44 +8,39 @@ import { PlanosContaComponent } from './area-logada/planos-conta/planos-conta.co
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { Erro404Component } from './erro404/erro404.component';
 import { LoginComponent } from './login/login.component';
+import { EstaLogadoGuard } from './shared/guards/esta-logado/esta-logado.guard';
+import { NaoEstaLogadoGuard } from './shared/guards/nao-esta-logado/nao-esta-logado.guard';
 
-const routes: Routes = [
-
-  {
-    path:'',
-    component:AreaLogadaComponent,
-    children:[{
-      path:'home',
-      component:HomeComponent
-    }]
-  },
-
-{
-
-  path:'home',
-  component:HomeComponent,
-
-
-
-},{
-  path:'login',
-  component:LoginComponent,
-
-},{
-  path:'planos-conta',
-  component:PlanosContaComponent,
-},{
-  path:'lancamentos',
-  component:LancamentosComponent,
-},{
-  path:'cadastro',
-  component:CadastroComponent
-},{
-  path:'**',
+const routes: Routes = [{
+  path: '',
+  component: AreaLogadaComponent,
+  canActivate: [EstaLogadoGuard],
+  children: [{
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }, {
+    path: 'home',
+    component: HomeComponent
+  }, {
+    path: 'planos-conta',
+    component: PlanosContaComponent
+  }, {
+    path: 'lancamentos',
+    component: LancamentosComponent
+  }]
+}, {
+  path: 'cadastro',
+  component: CadastroComponent,
+  canActivate: [NaoEstaLogadoGuard],
+}, {
+  path: 'login',
+  component: LoginComponent,
+  canActivate: [NaoEstaLogadoGuard],
+}, {
+  path: '**',
   component: Erro404Component
-}
-
-];
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
