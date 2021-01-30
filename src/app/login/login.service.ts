e6363772837dd76637307814376553b6ccb0c715
Raul Castro <raulc27@gmail.com>
2021-01-30 16:43:58 -0300

    consertando...

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
+import { AuthService } from '../shared/services/auth/auth.service';
 import { LoginCredenciais, LoginResponse } from './login.interfaces';
-
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +13,16 @@ export class LoginService {
 
   constructor(
     private http: HttpClient,
+    private authService: AuthService,
   ) { }
 
   logar(credenciais: LoginCredenciais): Observable<LoginResponse> {
-    return this.http.post<LoginResponse>('https://accenture-java-desafio.herokuapp.com/login', credenciais);
+    return this.http.post<LoginResponse>('https://accenture-java-desafio.herokuapp.com/login', credenciais)
+      .pipe(
+        tap(response => {
+          this.authService.setUsuario(response.usuario);
+          this.authService.setToken(response.token);
+        })
+      );
   }
 }
