cc410ffa2dcd4ef2253d2a948668af0be3426d18
Raul Castro <raulc27@gmail.com>
2021-01-29 21:01:12 -0300

    exerccicio diretivas

--- src/app/exercicio-diretivas/exercicio-diretivas.component.ts
+++ src/app/exercicio-diretivas/exercicio-diretivas.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 
+import { MEMES_AGRUPADOS_POR_CATEGORIA } from './exercicio-diretivas-constantes';
+
 @Component({
   selector: 'app-exercicio-diretivas',
   templateUrl: './exercicio-diretivas.component.html',
   styleUrls: ['./exercicio-diretivas.component.scss']
 })
+
 export class ExercicioDiretivasComponent {
 
-  deveExibir=true;
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+
+MEMES_AGRUPADOS_POR_CATEGORIA=MEMES_AGRUPADOS_POR_CATEGORIA;
+IMAGEM_PREFIXO="https://raw.githubusercontent.com/vitorfgsantos/angular-memes-diretivas/master/images/";
+
+  deveExibir=true;
+  
+  frutas=[
+    'Abacaxi',
+    'Laranja',
+    'Melancia',
+  ]
 
 trocarValor(){
   this.deveExibir=!this.deveExibir;
